68c1b06c4213f2c3d4eed9a61595799bf9c63aa6
sumit barsker <sumitbarsker876@gmail.com>
2026-06-18 20:37:17 +0530

    Implement solution for Best Time to Buy and Sell Stock

---
+++ Arrays/best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,14 @@
+# Problem: Best Time to Buy and Sell Stock
+# Platform: LeetCode
+# Difficulty: Easy
+
+class Solution:
+    def maxProfit(self, prices):
+        min_price = prices[0]
+        max_profit = 0
+
+        for price in prices:
+            min_price = min(min_price, price)
+            max_profit = max(max_profit, price - min_price)
+
+        return max_profit

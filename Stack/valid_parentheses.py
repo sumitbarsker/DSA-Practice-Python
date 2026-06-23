7ea0ed892a08a003e06c9280d198fe46fb8c4265
sumit barsker <sumitbarsker876@gmail.com>
2026-06-23 22:25:58 +0530

    Add valid parentheses checker in valid_parentheses.py
    Implement a solution to check for valid parentheses using a stack.

---
+++ Stack/valid_parentheses.py
@@ -0,0 +1,23 @@
+# Problem: Valid Parentheses
+# Platform: LeetCode
+# Difficulty: Easy
+
+class Solution:
+    def isValid(self, s):
+        stack = []
+        mapping = {
+            ')': '(',
+            '}': '{',
+            ']': '['
+        }
+
+        for char in s:
+            if char in mapping:
+                top = stack.pop() if stack else '#'
+
+                if mapping[char] != top:
+                    return False
+            else:
+                stack.append(char)
+
+        return not stack

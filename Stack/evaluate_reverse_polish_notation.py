5cab8f2e5db13cde9c405695300c04a1bf680d9d
sumit barsker <sumitbarsker876@gmail.com>
2026-06-30 19:42:33 +0530

    Implement Reverse Polish Notation evaluator

---
+++ Stack/evaluate_reverse_polish_notation.py
@@ -0,0 +1,29 @@
+# Problem: Evaluate Reverse Polish Notation
+# Platform: LeetCode
+# Difficulty: Medium
+
+class Solution:
+    def evalRPN(self, tokens):
+        stack = []
+
+        for token in tokens:
+            if token == "+":
+                b = stack.pop()
+                a = stack.pop()
+                stack.append(a + b)
+            elif token == "-":
+                b = stack.pop()
+                a = stack.pop()
+                stack.append(a - b)
+            elif token == "*":
+                b = stack.pop()
+                a = stack.pop()
+                stack.append(a * b)
+            elif token == "/":
+                b = stack.pop()
+                a = stack.pop()
+                stack.append(int(a / b))
+            else:
+                stack.append(int(token))
+
+        return stack[-1]

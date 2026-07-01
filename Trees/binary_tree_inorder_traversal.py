eb84b2837c8c48dde99a32bd0eb34ec814aa7448
sumit barsker <sumitbarsker876@gmail.com>
2026-07-01 14:35:31 +0530

    Implement binary tree inorder traversal

---
+++ Trees/binary_tree_inorder_traversal.py
@@ -0,0 +1,18 @@
+# Problem: Binary Tree Inorder Traversal
+# Platform: LeetCode
+# Difficulty: Easy
+
+class Solution:
+    def inorderTraversal(self, root):
+        result = []
+
+        def inorder(node):
+            if not node:
+                return
+
+            inorder(node.left)
+            result.append(node.val)
+            inorder(node.right)
+
+        inorder(root)
+        return result

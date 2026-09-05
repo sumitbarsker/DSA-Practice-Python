147acff896e2257cc26215a944c648ffb99dc0e5
sumit barsker <sumitbarsker876@gmail.com>
2026-09-05 20:46:59 +0530

    Add solution for intersection of two arrays

---
+++ Hashing/intersection_of_two_arrays.py
@@ -0,0 +1,10 @@
+# Problem: Intersection of Two Arrays
+# Platform: LeetCode
+# Difficulty: Easy
+
+class Solution:
+    def intersection(self, nums1, nums2):
+        set1 = set(nums1)
+        set2 = set(nums2)
+
+        return list(set1 & set2)

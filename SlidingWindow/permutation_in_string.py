2d9e771820bb6770b09535279d8bebc80f7fdb60
sumit barsker <sumitbarsker876@gmail.com>
2026-09-17 19:13:00 +0530

    Create permutation_in_string.py

---
+++ SlidingWindow/permutation_in_string.py
@@ -0,0 +1,25 @@
+# Problem: Permutation in String
+# Platform: LeetCode
+# Difficulty: Medium
+
+class Solution:
+    def checkInclusion(self, s1, s2):
+        if len(s1) > len(s2):
+            return False
+
+        count1 = [0] * 26
+        count2 = [0] * 26
+
+        for char in s1:
+            count1[ord(char) - ord('a')] += 1
+
+        for i in range(len(s2)):
+            count2[ord(s2[i]) - ord('a')] += 1
+
+            if i >= len(s1):
+                count2[ord(s2[i - len(s1)]) - ord('a')] -= 1
+
+            if count1 == count2:
+                return True
+
+        return False
